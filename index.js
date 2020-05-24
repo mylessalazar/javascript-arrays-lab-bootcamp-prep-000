b14c3d36b3ec91f828c96b572b6a332f432396e4
Myles <mylesssalazar@gmail.com>
2020-05-24 15:10:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ function prependKitten(name){
 }
 function removeLastKitten(){
   return kittens.slice(){
-return kittens.slice(0,kittens.length-1)    
+return kittens.slice(0,kittens.length-1)
   }
